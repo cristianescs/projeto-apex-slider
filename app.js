a7c0146d11a4495dace39ee454b43b147d2f3f0c
Cristiane Silva <cristiane.silvac@hotmail.com>
2022-08-09 14:28:56 -0300

    Projeto finalizado

---
+++ app.js
@@ -0,0 +1,34 @@
+const btnAvancar = $('#btn-avancar');
+const btnVoltar = $('#btn-voltar');
+var cartoes = $('.cartao');
+var cartaoAtual = 0;
+
+
+function esconderCartaoSelecionado(){
+    $('.selecionado').removeClass('selecionado');
+}
+
+function mostrarCartao(){
+    $(cartoes[cartaoAtual]).addClass('selecionado');
+}
+
+
+btnAvancar.click(function () {
+    if (cartaoAtual === cartoes.length - 1){
+        cartaoAtual = cartoes.length - (cartoes.length + 1);
+    }
+
+    esconderCartaoSelecionado();
+    cartaoAtual++;
+    mostrarCartao()
+});
+
+btnVoltar.click(function () {
+    if (cartaoAtual === 0){
+        cartaoAtual = cartoes.length;
+    }
+
+    esconderCartaoSelecionado();
+    cartaoAtual--;
+    mostrarCartao()
+});
